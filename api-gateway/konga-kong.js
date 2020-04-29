3bac7e75189a36c3698b3e81b752c94cfda87e6a
Joachim Schirrmacher <joachimschirrmacher@gmail.com>
2020-04-29 18:19:24 +0200

    Export from current cluster

---
+++ api-gateway/konga-kong.js
@@ -0,0 +1,7 @@
+module.exports = [{
+  "name": "Germanzero API Gateway",
+  "type": "key_auth",
+  "kong_admin_url": "http://kong:8001",
+  "kong_api_key": "<api-key>",
+  "health_checks": false,
+}]
---
+++ api-gateway/konga-users.js
@@ -0,0 +1,10 @@
+module.exports = [{
+  "username": "<username>",
+  "email": "<username>@germanzero.de",
+  "firstName": "My",
+  "lastName": "Name",
+  "node_id": "http://kong:8001",
+  "admin": true,
+  "active" : true,
+ "password": "<user-password>"
+}]
